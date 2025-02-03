a6ab51c4c1156053955dbe5110ed175881a40d68
Drashti33 <drashtimodi33@gmail.com>
2025-02-03 09:09:44 +0530

    Program

---
+++ PYTHON_CLASS_WORK/Basic/Hello.py
@@ -0,0 +1,19 @@
+# terminal ctrl+~=Terminal, tab thi name ave se file nu
+# cd.. file ni bhare nikdya
+# PS C:\Tops_Work\PythonProgram> cd .\Basic\
+# PS C:\Tops_Work\PythonProgram\Basic> py .\Hello.py
+# Hello Python!
+
+print ('Hello Python')
+print("Hello Python!")
+print("""Hello Everyone!
+Wellcome""")
+print("Hello ' Python! '")
+print('Hello " Python! "')
+print("Hello \" Python! \"")
+print('Hello \' Python! \'')
+print("Hello \\ Python! \\")
+print("Hello \ Python! \ ")
+print("Hello \n Wellcome")
+print("Hello \t Wellcome")
+print("Hello!\b Python!")
