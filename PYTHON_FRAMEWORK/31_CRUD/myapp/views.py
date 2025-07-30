ac01ff00a8d690ebd3370a9bf76fd6a1cd280600
Drashti33 <drashtimodi33@gmail.com>
2025-07-30 20:03:46 +0530

    Python

--- PYTHON_FRAMEWORK/31_CRUD/myapp/views.py
+++ PYTHON_FRAMEWORK/31_CRUD/myapp/views.py
@@ -12,28 +12,26 @@ def register(request):
     if request.method == 'POST':
         data = request.POST
         # print(data)
+        stid = data.get('id')     # Get ID from form (if present)
         name = data.get('name')
         email = data.get('email')
         phone = data.get('phone')
         age = data.get('age')
-        image = request.FILES.get('file')  # Get the uploaded file from the form
-        
-        
-        stid = data.get('stid')  # Get ID from form (if present)
+        image = request.FILES.get['file']  # Get the uploaded file from the form
 
-        if stid:  # If stid is provided, it means update
-            Student = student.objects.get(pk=stid)
+        if id:  # If stid is provided, it means update
+            Student = student.objects.get(pk=id)
             Student.name=name
             Student.email= email
             Student.phone=phone
-            Student.age=age
+            Student.age=age    
             os.remove(Student.image.path)  # Remove the old image file
-            Student.image=request.FILES.get('file')  # Update the image if a new one is uploaded
+            Student.image=request.FILES.get['file']  # Update the image if a new one is uploaded
             Student.save()
             return redirect("home")
         else:
             st =  student.objects.create(name=name,email=email,phone=phone,age=age,image=image)
-            if(st):
+            if st:
                 #return render(request,'home.html',{"msg":"Registration successful"})
                 return redirect("home")
 
@@ -41,14 +39,14 @@ def register(request):
 
 
 def delete(request):
-    stid =  request.GET['stid']
-    Student = student.objects.get(pk=stid)
+    id =  request.GET['id']
+    Student = student.objects.get(pk=id)
     os.remove(Student.image.path)  # Delete the image file from the filesystem
     Student.delete()
     return redirect("home")
 
 def update(request):
-    stid =  request.GET['stid']
-    Student = student.objects.get(pk=stid)
+    id =  request.GET['id']
+    Student = student.objects.get(pk=id)
     allstudents  = student.objects.all()
     return render(request,"home.html", {"Student": Student, "students": allstudents})
